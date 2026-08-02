0f0c5569050d2c43765b312f9c5e5a35f16436dc
Petra <petra.paloniemi94@hotmail.com>
2026-08-02 14:30:18 +0300

    Improved responsiveness for notifications

--- front/quiz-app/src/components/Notification.tsx
+++ front/quiz-app/src/components/Notification.tsx
@@ -8,7 +8,7 @@ const Notification = () => {
     return null;
   }
   return (
-    <div className="overlay w-fit md:max-w-1/3 px-4 py-2 rounded border-2 border-gray-300 text-center font-semibold">
+    <div className="overlay w-fit md:max-w-1/3 wrap-anywhere px-4 py-2 rounded border-2 border-gray-300 text-center font-semibold">
       {message}
     </div>
   );
